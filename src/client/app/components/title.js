fd4bd3e3f715764a65113c94f1ca4e1d4b7b1d1a
samlovescoffee <sam@intravenous.coffee>
2017-05-23 22:42:20 +0100

    Try more elements

--- src/client/app/components/title.js
+++ src/client/app/components/title.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-export const title = (text) => <h1> {text} </h1>;
-
+export const title = (text, sub) => (
+	<div>
+		<h1> {text} </h1>
+		<h2> {sub} </h2>
+	</div>
+)
 
 export default title
