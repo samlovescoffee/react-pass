10dc25237461a88d734771394170345a3c1892d4
samlovescoffee <sam@intravenous.coffee>
2017-06-28 23:08:06 +0100

    bin off comment

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
 	res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-//and remove caching so we get the most recent users
+	//and remove caching so we get the most recent users
 	res.setHeader('Cache-Control', 'no-cache');
 	next();
 });
